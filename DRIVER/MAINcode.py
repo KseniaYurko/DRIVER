9dd3ba8faba14091cb4653fc5b441ac3c6b9b7c4
KseniaYurko <iurko.kk@phystech.edu>
2021-04-16 22:10:52 +0300

    Rename

--- DRIVER/MAINcode.py
+++ DRIVER/MAINcode.py
@@ -1,10 +1,13 @@
 import os
 import datetime
+import spidev
 
 path = r'/home/pi/ADC' 
-
 now = datetime.datetime.now()
 DATE = now.strftime("%d.%m.%Y-%H:%M:%S")
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
 
 try:
     os.mkdir(path)
